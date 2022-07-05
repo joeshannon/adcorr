38155135c96aac30bf2e2eb54b27ebabd809c03e
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-05 13:14:21 +0000

    Add tests for masking

---
+++ tests/test_masking.py
@@ -0,0 +1,31 @@
+from numpy import Inf, array
+from pytest import raises
+
+from adcorr.masking import mask_frames
+
+
+def test_masking_typical_3x3():
+    assert (
+        array([[Inf, 2.0], [3.0, Inf]])
+        == mask_frames(
+            array([[1.0, 2.0], [3.0, 4.0]]), array([[True, False], [False, True]])
+        ).filled(Inf)
+    ).all()
+
+
+def test_masking_typical_2x3x3():
+    assert (
+        array([[[Inf, 2.0], [3.0, Inf]], [[Inf, 6.0], [7.0, Inf]]])
+        == mask_frames(
+            array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]]),
+            array([[True, False], [False, True]]),
+        ).filled(Inf)
+    ).all()
+
+
+def test_masking_non_broadcastable_mask_raises():
+    with raises(ValueError):
+        mask_frames(
+            array([[1.0, 2.0], [3.0, 4.0]]),
+            array([[True, False, True], [False, True, False]]),
+        )
