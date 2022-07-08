0a3269bf8191a8fed29da8c4192dd5a859785f25
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-08 13:15:35 +0000

    Fix normalize_frame_time bounds checking

--- src/adcorr/frame_time.py
+++ src/adcorr/frame_time.py
@@ -1,17 +1,17 @@
-from typing import Any, TypeVar
+from typing import Literal, Tuple, TypeVar, Union
 
 from numpy import atleast_1d, dtype, expand_dims, floating, ndarray
-from numpy.ma import MaskedArray, masked_array
 
 FrameDType = TypeVar("FrameDType", bound=dtype)
-FramesShape = TypeVar("FramesShape", bound=Any)
-TimesShape = TypeVar("TimesShape", bound=Any)
+NumFrames = TypeVar("NumFrames", bound=int)
+FrameWidth = TypeVar("FrameWidth", bound=int)
+FrameHeight = TypeVar("FrameHeight", bound=int)
 
 
 def normalize_frame_time(
-    frames: MaskedArray[FramesShape, FrameDType],
-    count_times: ndarray[TimesShape, dtype[floating]],
-) -> MaskedArray[FramesShape, FrameDType]:
+    frames: ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType],
+    count_times: ndarray[Tuple[Union[NumFrames, Literal[1]]], dtype[floating]],
+) -> ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]:
     """Normalize for detector frame rate by scaling photon counts according to count time.
 
     Normalize for detector frame rate by scaling photon counts according to count time,
@@ -19,13 +19,17 @@ def normalize_frame_time(
     collection and correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
-        frames (MaskedArray[FramesShape, FrameDType]): A stack of frames to be
-            normalized.
-        count_times (ndarray[TimesShape, dtype[floating]]): The period over which
-            photons are counted for each frame.
+        frames (ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]): A
+            stack of frames to be normalized.
+        count_times (ndarray[Tuple[Union[TimesShape, Literal[1]]], dtype[floating]]):
+            The period over which photons are counted for each frame.
 
     Returns:
-        MaskedArray[FramesShape, FrameDType]: The normalized stack of frames.
+        ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]: The normalized
+            stack of frames.
     """
-    times = expand_dims(atleast_1d(count_times), (-2, -1))
-    return masked_array(frames.data / times, frames.mask)
+    if (count_times <= 0).any():
+        raise ValueError("Count times must be positive.")
+
+    times = expand_dims(atleast_1d(count_times), (1, 2))
+    return frames / times
