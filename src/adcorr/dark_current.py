c1efb4fdcaf6239b88215fb67a8580021e24619a
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-08 12:45:28 +0000

    Fix correct_dark_current bounds checking

--- src/adcorr/dark_current.py
+++ src/adcorr/dark_current.py
@@ -1,7 +1,6 @@
-from typing import Tuple, TypeVar
+from typing import Literal, Tuple, TypeVar, Union
 
-from numpy import array, dtype, expand_dims, floating, ndarray
-from numpy.ma import MaskedArray, masked_array
+from numpy import atleast_1d, dtype, expand_dims, floating, ndarray
 
 FrameDType = TypeVar("FrameDType", bound=dtype)
 NumFrames = TypeVar("NumFrames", bound=int)
@@ -10,12 +9,12 @@ FrameHeight = TypeVar("FrameHeight", bound=int)
 
 
 def correct_dark_current(
-    frames: MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType],
-    count_times: ndarray[Tuple[NumFrames], dtype[floating]],
+    frames: ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType],
+    count_times: ndarray[Tuple[Union[NumFrames, Literal[1]]], dtype[floating]],
     base_dark_current: float,
     temporal_dark_current: float,
     flux_dependant_dark_current: float,
-) -> MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]:
+) -> ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]:
     """Correct by subtracting base, temporal and flux-dependant dark currents.
 
     Correct for incident dark current by subtracting a baselike, time dependant and a
@@ -24,20 +23,28 @@ def correct_dark_current(
     [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
-        frames (MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]): A
+        frames (ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]): A
             stack of frames to be corrected.
-        atemporal_dark_current (float): The dark current flux, irrespective of time.
+        count_times (ndarray[Tuple[Union[NumFrames, Literal[1]]], dtype[floating]]):
+            The period over which photons are counted for each frame.
+        base_dark_current (float): The dark current flux, irrespective of time.
         temporal_dark_current (float): The dark current flux, as a factor of time.
         flux_dependant_dark_current (float): The dark current flux, as a factor of
             incident flux.
-        count_times (ndarray[Tuple[NumFrames], dtype[floating]]): The period over which
-            photons are counted for each frame.
 
     Returns:
-        MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]: The
+        ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]: The
             corrected stack of frames.
     """
-    base = array([(base_dark_current, 0.0)])
-    temporal = expand_dims(temporal_dark_current * count_times, (-2, -1))
+    if (count_times <= 0).any():
+        raise ValueError("Count times must be positive.")
+    if base_dark_current < 0:
+        raise ValueError("Base Dark Current must be non-negative.")
+    if temporal_dark_current < 0:
+        raise ValueError("Temporal Dark Current must be non-negative.")
+    if flux_dependant_dark_current < 0:
+        raise ValueError("Flux Dependant Dark Current must be non-negative.")
+
+    temporal = temporal_dark_current * expand_dims(atleast_1d(count_times), (1, 2))
     flux_dependant = flux_dependant_dark_current * frames
-    return masked_array(frames.data - base - temporal - flux_dependant, frames.mask)
+    return frames - base_dark_current - temporal - flux_dependant
