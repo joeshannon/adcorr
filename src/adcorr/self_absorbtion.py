c3742874bda12d50e0ee2ed7e84cda768a5cd66c
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-11 12:52:22 +0000

    Fix correct_self_absorbtion for edge cases

--- src/adcorr/self_absorbtion.py
+++ src/adcorr/self_absorbtion.py
@@ -1,7 +1,17 @@
+from math import exp
 from typing import Tuple, TypeVar, cast
 
-from numpy import cos, dtype, exp, floating, log, ndarray, power
-from numpy.ma import MaskedArray, masked_array
+from numpy import (
+    cos,
+    divide,
+    dtype,
+    floating,
+    log,
+    logical_and,
+    ndarray,
+    ones_like,
+    power,
+)
 
 from .utils.geometry import scattering_angles
 
@@ -11,44 +21,14 @@ FrameWidth = TypeVar("FrameWidth", bound=int)
 FrameHeight = TypeVar("FrameHeight", bound=int)
 
 
-def self_absorbtion_correction_factors(
-    frame_shape: Tuple[FrameWidth, FrameHeight],
-    beam_center: Tuple[float, float],
-    pixel_sizes: Tuple[float, float],
-    distance: float,
-    absorbtion_coefficient: float,
-    thickness: float,
-) -> ndarray[Tuple[FrameWidth, FrameHeight], dtype[floating]]:
-    """Computes the self absorbtion correction factors given geometry and transmissibility.
-
-    Args:
-        frame_shape (Tuple[FrameWidth, FrameHeight]): The shape of a frame.
-        beam_center (Tuple[float, float]): The center position of the beam in pixels.
-        pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
-        distance (float): The distance between the detector and the sample.
-        absorbtion_coefficient (float): The coefficient of absorbtion for a given
-            material at a given photon energy.
-        thickness (float): The thickness of the detector head material.
-
-    Returns:
-        ndarray[Any, dtype[floating]]: An array of correction factors to be applied to
-            frames.
-    """
-    angles = scattering_angles(frame_shape, beam_center, pixel_sizes, distance)
-    transmissibility = exp(-absorbtion_coefficient * thickness)
-    return (1 - power(transmissibility, 1 / cos(angles) - 1)) / (
-        log(transmissibility) * (1 - 1 / cos(angles))
-    )
-
-
 def correct_self_absorbtion(
-    frames: MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType],
+    frames: ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType],
     beam_center: Tuple[float, float],
     pixel_sizes: Tuple[float, float],
     distance: float,
     absorbtion_coefficient: float,
     thickness: float,
-) -> MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]:
+) -> ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]:
     """Correct for transmission loss due to differences in observation angle.
 
     Correct for transmission loss due to differences in observation angle, as detailed
@@ -56,7 +36,7 @@ def correct_self_absorbtion(
     correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
-        frames (MaskedArray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]): A
+        frames (ndarray[Tuple[NumFrames, FrameWidth, FrameHeight], FrameDType]): A
             stack of frames to be corrected.
         beam_center (Tuple[float, float]): The center position of the beam in pixels.
         pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
@@ -66,14 +46,23 @@ def correct_self_absorbtion(
         thickness (float): The thickness of the detector head material.
 
     Returns:
-        MaskedArray[FramesShape, FrameDType]: The corrected stack of frames.
+        ndarray[FramesShape, FrameDType]: The corrected stack of frames.
     """
-    correction_factors = self_absorbtion_correction_factors(
-        cast(Tuple[FrameWidth, FrameHeight], frames.shape[-2:]),
-        beam_center,
-        pixel_sizes,
-        distance,
-        absorbtion_coefficient,
-        thickness,
+    if absorbtion_coefficient < 0.0:
+        raise ValueError("Absorbtion coefficient must non-negative.")
+    if thickness < 0.0:
+        raise ValueError("Thickness must be non-negative.")
+
+    angles = scattering_angles(
+        cast(Tuple[int, int], frames.shape[-2:]), beam_center, pixel_sizes, distance
     )
-    return masked_array(frames.data * correction_factors, frames.mask)
+    transmissibility = exp(-absorbtion_coefficient * thickness)
+    secangle: ndarray[Tuple[int, int], dtype[floating]] = 1 / cos(angles)
+    correction_factors: ndarray[Tuple[int, int], dtype[floating]] = divide(
+        1 - power(transmissibility, secangle - 1),
+        log(transmissibility) * (1 - secangle),
+        out=ones_like(secangle),
+        where=logical_and(secangle != 1.0, transmissibility != 1.0),
+    )
+
+    return frames * correction_factors
