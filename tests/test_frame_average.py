461d7f543e073b5522fa735de96ae958acc43c5f
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-11 14:00:33 +0000

    Add masked tests to frame average

--- tests/test_frame_average.py
+++ tests/test_frame_average.py
@@ -1,4 +1,5 @@
-from numpy import array, isclose
+from numpy import Inf, array, isclose
+from numpy.ma import masked_where
 
 from adcorr import average_all_frames
 
@@ -40,4 +41,39 @@ def test_average_all_frames_typical_3x2x2():
                 ]
             ),
         ),
+    ).all()
+
+
+def test_average_all_frames_masked_2x2():
+    assert isclose(
+        array([[Inf, 2.0], [3.0, Inf]]),
+        average_all_frames(
+            masked_where(
+                array([[True, False], [False, True]]), array([[1.0, 2.0], [3.0, 4.0]])
+            )
+        ),
+    ).all()
+
+
+def test_average_all_frames_masked_2x2x2():
+    assert isclose(
+        array([[Inf, 8.0], [10.0, Inf]]),
+        average_all_frames(
+            masked_where(
+                array([[[True, False], [False, True]], [[True, False], [False, True]]]),
+                array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]]),
+            )
+        ).filled(Inf),
+    ).all()
+
+
+def test_average_all_frames_masked_diag_2x2x2():
+    assert isclose(
+        array([[5.0, 2.0], [3.0, 8.0]]),
+        average_all_frames(
+            masked_where(
+                array([[[True, False], [False, True]], [[False, True], [True, False]]]),
+                array([[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]]),
+            )
+        ).filled(Inf),
     ).all()
