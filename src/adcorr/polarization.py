6e2c66015ffbc080d6eec0b2892f663b6d72ec98
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-07-12 10:52:57 +0000

    Simplify correct_polarization computation

--- src/adcorr/polarization.py
+++ src/adcorr/polarization.py
@@ -1,7 +1,6 @@
-from typing import Tuple, TypeVar
+from typing import Tuple, TypeVar, cast
 
-from numpy import cos, dtype, sin, square
-from numpy.ma import MaskedArray, masked_array
+from numpy import cos, dtype, ndarray, sin, square
 
 from .utils.geometry import azimuthal_angles, scattering_angles
 
@@ -10,12 +9,12 @@ FramesShape = TypeVar("FramesShape", bound=Tuple[int, int, int])
 
 
 def correct_polarization(
-    frames: MaskedArray[FramesShape, FrameDType],
+    frames: ndarray[FramesShape, FrameDType],
     beam_center: Tuple[float, float],
     pixel_sizes: Tuple[float, float],
     distance: float,
     horizontal_poarization: float = 0.5,
-) -> MaskedArray[FramesShape, FrameDType]:
+) -> ndarray[FramesShape, FrameDType]:
     """Corrects for the effect of polarization of the incident beam.
 
     Corrects for the effect of polarization of the incident beam, as detailed in
@@ -23,8 +22,7 @@ def correct_polarization(
     correction' [https://doi.org/10.1088/0953-8984/25/38/383201].
 
     Args:
-        frames (MaskedArray[FramesShape, FrameDType]): A stack of frames to be
-            corrected.
+        frames (ndarray[FramesShape, FrameDType]): A stack of frames to be corrected.
         beam_center (Tuple[float, float]): The center position of the beam in pixels.
         pixel_sizes (Tuple[float, float]): The real space size of a detector pixel.
         distance (float): The distance between the detector and the sample.
@@ -33,13 +31,18 @@ def correct_polarization(
             source. Defaults to 0.5.
 
     Returns:
-        MaskedArray[FramesShape, FrameDType]: The corrected stack of frames.
+        ndarray[FramesShape, FrameDType]: The corrected stack of frames.
     """
+    if horizontal_poarization < 0.0 or horizontal_poarization > 1.0:
+        raise ValueError("Horizontal Polarization must be within the interval [0, 1].")
+
     scattering = scattering_angles(
-        frames.shape[-2:], beam_center, pixel_sizes, distance
+        cast(tuple[int, int], frames.shape[-2:]), beam_center, pixel_sizes, distance
     )
-    azimuths = azimuthal_angles(frames.shape[-2:], beam_center)
-    correction_factor = horizontal_poarization * (
-        1 - square(sin(azimuths) * sin(scattering))
-    ) + (1 - horizontal_poarization) * (1 - square(cos(azimuths) * sin(scattering)))
-    return masked_array(frames * correction_factor, frames.mask)
+    azimuths = azimuthal_angles(
+        cast(tuple[int, int], frames.shape[-2:]), beam_center, pixel_sizes
+    )
+    correction_factors = horizontal_poarization * (
+        1.0 - square(sin(azimuths) * sin(scattering))
+    ) + (1.0 - horizontal_poarization) * (1.0 - square(cos(azimuths) * sin(scattering)))
+    return frames * correction_factors
