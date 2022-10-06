92f55979a65054fe0d92bc960b97696bcd147906
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-10-06 12:23:00 +0000

    Update CLI message

--- src/adcorr/cli.py
+++ src/adcorr/cli.py
@@ -7,10 +7,9 @@ from . import __version__
 @click.version_option(version=__version__, message="%(version)s")
 @click.pass_context
 def main(ctx: click.Context) -> None:
-    """The main command line interface entry point.
+    """Area detector corrections as pure python functions.
 
-    Args:
-        ctx: The click context.
+    This project is intended to be used as a library, and thus has no CLI.
     """
     if ctx.invoked_subcommand is None:
         click.echo(main.get_help(ctx))
