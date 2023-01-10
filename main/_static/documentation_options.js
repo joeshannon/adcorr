8dc98fce6491c91630b487ede0b6fb5260f14fd4
garryod <garryod@users.noreply.github.com>
2023-01-10 09:53:13 +0000

    deploy: 16cf52f998936b2f07e4c536cb5cb0798a495f46

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.2.dev3+ga095e8d',
+    VERSION: '0.2.2.dev4+g16cf52f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
